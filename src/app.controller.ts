2ce87edb49ac8dfbd33710707147c4dcecbc175e
rocio <kimhr001@hanmail.net>
2025-05-28 11:57:55 +0900

    misc : remove hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('.ping')
+  @ApiOperation({ summary: 'Ping' })
+  @ApiResponse({ status: 200, description: 'Ping' })
   ping(): string {
     return this.appService.ping();
   }
